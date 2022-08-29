14e84f8b641923f968319ccf10f75a78533d3239
Ramiro Ramirez-Sanchez <45070898+ramirors1@users.noreply.github.com>
2022-08-29 21:21:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+# one = means it is assigning a value to a variable
+# two == checks if the value on the left is = to the value on the right
+if height >= 120:
+  print("Enjoy the ride?")
+else:
+  print("Sorry shorty, you can't ride this ride.") 
